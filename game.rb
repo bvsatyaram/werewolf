ce2f793abf9e365f774e12d5000d95c9b1c0874a
B V Satyaram <bvsatyaram@gmail.com>
2015-06-11 16:32:30 +0530

    Game can access players list

--- game.rb
+++ game.rb
@@ -1,3 +1,5 @@
+require_relative 'player'
+
 class Game
   def initialize(no_of_wolves, no_of_villagers)
     @players = []
@@ -5,7 +7,11 @@ class Game
       @players.push(Player.new(true))
     end
     no_of_villagers.times do
-      @players.push(Player.new(false))
+      @players.push(Player.new)
     end
+  end
+
+  def players
+    return @players
   end
 end
---
+++ player.rb
@@ -0,0 +1,5 @@
+class Player
+  def initialize(is_wolf = false)
+    @is_wolf = is_wolf    
+  end
+end
