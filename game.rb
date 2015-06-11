ed4d66f2b56cd664772c46b2f03e42178146b903
B V Satyaram <bvsatyaram@gmail.com>
2015-06-11 16:22:03 +0530

    Using only one class for now

--- game.rb
+++ game.rb
@@ -2,10 +2,10 @@ class Game
   def initialize(no_of_wolves, no_of_villagers)
     @players = []
     no_of_wolves.times do
-      @players.push(Wolf.new)
+      @players.push(Player.new(true))
     end
     no_of_villagers.times do
-      @players.push(Villager.new)
+      @players.push(Player.new(false))
     end
   end
 end
