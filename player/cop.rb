543cf1b93dba75bfcd95b499ad7dd9dbc7abe788
vineetsouri <vineetsouri72@gmail.com>
2015-06-15 15:17:50 +0530

    added COP class in werewolf game

---
+++ player/cop.rb
@@ -0,0 +1,18 @@
+require_relative "player"
+class Cop < Villager
+  def intialize(game)
+    super(game)
+    @role = Player::Role::COP
+  end
+  def indentify_player
+    if self.alive?
+      @identified_player = @players.alive.sample
+      if @identified_player.wolf?
+        $logger.log "Cop has identified a wolf #{@identified_player}"
+      else
+        $logger.log "Cop has identified a villager #{@identified_player}"
+      end
+      return @identified_player
+    end
+  end
+end
